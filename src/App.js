9cc60c5e085efb48214c16f927857226ab0d3244
JacobPaul710 <jacobpaulscott@outlook.com>
2023-05-27 17:26:33 -0700

    Creates simple Home component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MineralIndex from './pages/MineralIndex';
 import MineralShow from './pages/MineralShow';
 import MineralNew from './pages/MineralNew';
+import MineralHome from './pages/MineralHome';
 import Nav from './components/Nav';
 import { Routes, Route } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ function App() {
     <>
      <Nav />
      <Routes>
+        <Route path='/' element={<MineralHome />} />
         <Route path='/minerals' element={<MineralIndex />} />
         <Route path='/minerals'>
           <Route path=':id' element={<MineralShow />} />
---
+++ src/pages/MineralHome.js
@@ -0,0 +1,6 @@
+function MineralHome() {
+    return(
+    <h1>Welcome to Oregon's Houding Tracker!</h1>
+)}
+
+export default MineralHome;
