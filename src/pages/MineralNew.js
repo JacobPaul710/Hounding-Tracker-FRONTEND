4b89a2971b6361fb458462760444f30ebd309217
JacobPaul710 <jacobpaulscott@outlook.com>
2023-05-29 12:44:08 -0700

    Creates component that allows new minerals to be added to the database using React forms. Also updates readme.md.

--- src/pages/MineralNew.js
+++ src/pages/MineralNew.js
@@ -12,12 +12,14 @@ function NewMineral() {
    }) 
 
    function handleChange(e) {
-    console.log(e.target);
+    console.log(e.target, "e target");
     setMineralForm((previousFormState) => ({
         ...previousFormState,
         [e.target.name]: e.target.value
     }))
    }
+
+   console.log(mineralForm, "Books Form");
 
    async function handleSubmit(e) {
     try{
@@ -41,16 +43,22 @@ function NewMineral() {
     <form onSubmit={handleSubmit}>
         <label>Mineral Type:</label>
         <input type="text" name="mineralType" onChange={handleChange} placeholder="Mineral Type"/>
+        <br></br>
         <label>Date:</label>
         <input type="text" name="date" onChange={handleChange} placeholder="Date of find"/>
+        <br></br>
         <label>Conditions:</label>
         <input type="text" name="conditions" onChange={handleChange} placeholder="Conditions on day of find"/>
+        <br></br>
         <label>County:</label>
         <input type="text" name="county" onChange={handleChange} placeholder="County of find"/>
+        <br></br>
         <label>Image:</label>
         <input type="text" name="image" onChange={handleChange} placeholder="Paste image URL"/>
+        <br></br>
         <label>Hounding Name:</label>
         <input type="text" name="houndingName" onChange={handleChange} placeholder="Name of hounder(username or real)"/>
+        <br></br>
         <button>Submit</button>
     </form>
     )
